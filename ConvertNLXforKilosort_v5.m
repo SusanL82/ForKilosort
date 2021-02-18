803f3766a4f9766942b95a4cd931ad6122fffcd9
Susan <78316402+SusanL82@users.noreply.github.com>
2021-02-18 10:55:54 +0100

    Update ConvertNLXforKilosort_v5.m
    Added option to define/exclude bad channels, made number of channels variable

--- ConvertNLXforKilosort_v5.m
+++ ConvertNLXforKilosort_v5.m
@@ -1,42 +1,48 @@
 %% Convert NLX data to .dat for Kilosort.m
 
-function [RatID,RecDate] = ConvertNLXforKilosort_v5(InFolder,OutFolder,RatID,RecDate,MakeMat)
+function [RatID,RecDate] = ConvertNLXforKilosort_v6(InFolder,OutFolder,RatID,RecDate,BadChans,MakeMat)
+%% settings for read/write .dat file
+fs = 20000; %sampling rate
+Chunk = 10; %chunklength in minutes
+ChunkLen = Chunk*60*fs;  %chunklength in samples
+NumChans = 64;
 
+%% make list of good channels
+GoodChans = setdiff([1:NumChans],BadChans);
+
+
+%% save signal from each good channel as .mat file.
 if MakeMat ==1
-    %% save signal from each channel as .mat file.
+    
     % make TempFolder for .mat files if there isn't one yet
     if exist([OutFolder,'\','Temp']) == 0
         mkdir([OutFolder,'\','Temp']);
     end
     
-    for ch = 1:64
+    for ch = 1:numel(GoodChans)
         % convert .ncs to mat
-        InFile = [InFolder,'\','CSC',num2str(ch),'.ncs'];
+        InFile = [InFolder,'\','CSC',num2str(GoodChans(ch)),'.ncs'];
         [~,~,samples] = readEegDataForKilosort(InFile); % gives 1D array of type double
         samples = int16(samples);
         save([OutFolder,'\','Temp','\',RatID,'_',RecDate,'_ch',num2str(ch),'.mat'],'samples','-v7.3')
     end
     
 else
-    load([OutFolder,'\','Temp','\',RatID,'_',RecDate,'_ch1','.mat'],'samples')
+    load([OutFolder,'\','Temp','\',RatID,'_',RecDate,'_ch',num2str(GoodChans(1)),'.mat'],'samples') %load first channel to get rec length
 end
+
 %% read data from all channels per chunk, write to .dat
-fs = 20000; %sampling rate
-Chunk = 10; %chunklength in minutes
-ChunkLen = Chunk*60*fs;  %chunklength in samples
 RecLen = length(samples); %get recording length
 NumChunks = floor(RecLen/ChunkLen);
-
-
 
 for i = 1:NumChunks
     disp(['chunk ',num2str(i),' of ',num2str(NumChunks+1)])
     
-    AllChunks=zeros(64,ChunkLen);AllChunks=int16(AllChunks);
-    for ch = 1:64
-        load([OutFolder,'\','Temp','\',RatID,'_',RecDate,'_ch',num2str(ch),'.mat'],'samples');
+    AllChunks=zeros(NumChans,ChunkLen);AllChunks=int16(AllChunks);
+    for ch = 1:numel(GoodChans)
+        load([OutFolder,'\','Temp','\',RatID,'_',RecDate,'_ch',num2str(GoodChans(ch)),'.mat'],'samples');
         SampChunk = samples((i*ChunkLen)+1-ChunkLen:i*ChunkLen);
-        AllChunks(ch,:)=SampChunk';
+        AllChunks(GoodChans(ch),:)=SampChunk';
         clear samples
     end
     
@@ -60,11 +66,11 @@ disp(['chunk ',num2str(NumChunks+1),' of ',num2str(NumChunks+1)])
 % write remaining data to .dat
 if RecLen > (NumChunks*ChunkLen)
     RecLeft = RecLen-(NumChunks*ChunkLen);
-    LastChunks = zeros(64,RecLeft);LastChunks = int16(LastChunks); 
-    for ch = 1:64
-        load([OutFolder,'\','Temp','\',RatID,'_',RecDate,'_ch',num2str(ch),'.mat'],'samples');
+    LastChunks = zeros(NumChans,RecLeft);LastChunks = int16(LastChunks); 
+    for ch = 1:NumChans
+        load([OutFolder,'\','Temp','\',RatID,'_',RecDate,'_ch',num2str(GoodChans(ch)),'.mat'],'samples');
         SampChunk = samples(NumChunks*ChunkLen+1:end);
-        LastChunks(ch,:)=SampChunk';
+        LastChunks(GoodChans(ch),:)=SampChunk';
     end
     
     fid = fopen(Outfile, 'a');
