0cc0e8e9da47e90c2150656ecaace0e8e3306d1d
Susan <78316402+SusanL82@users.noreply.github.com>
2021-02-18 10:58:22 +0100

    Update BatchConvertNLXforKilosort.m
    Updated to include BadChans

--- BatchConvertNLXforKilosort.m
+++ BatchConvertNLXforKilosort.m
@@ -16,7 +16,7 @@ disp(['file ',num2str(f),' of ',num2str(size(InFiles{1},1))])
 disp(['processing ',InFolder])
 
 %[~,~] = ConvertNLXforKilosort_v2(InFolder,OutFolder,RatID,RecDate);
-[~,~] = ConvertNLXforKilosort_v5(InFolder,OutFolder,RatID,RecDate,MakeMat);
+[~,~] = ConvertNLXforKilosort_v5(InFolder,OutFolder,RatID,RecDate,BadChans,MakeMat);
 disp(['completed ',InFolder])
 
 end
